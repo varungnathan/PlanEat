bd6077b6aa6126437766d73ffbc39fe5cf4020e8
combarathaju <taju77366@gmail.com>
2024-09-30 14:00:02 -0400

    Navbar added

---
+++ plan-eat-frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/search">Recipe Search</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
